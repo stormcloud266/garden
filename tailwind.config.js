e7261feac27cfa1c4fd957847260cf5c9f0b38eb
Tawnee <allprt89@gmail.com>
2022-08-26 21:21:19 -0600

    update configs, add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        slate: {
+          DEFAULT: "#3d4958",
+          light: "#405064",
+        },
+        "off-white": "#fff8f0",
+        burnt: "#b2675e",
+      },
+      fontFamily: {
+        title: ["'Averia Serif Libre'", "serif"],
+        body: ["'Open Sans'", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
