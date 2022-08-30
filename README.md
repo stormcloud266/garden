122681f53082523f6fbe1fc81f0b5dd89b8343c1
Tawnee <allprt89@gmail.com>
2022-08-29 19:22:33 -0600

    add syntax highlighting to md-components

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 - Features links on homepage
 - All links page
 - Filter by category
-- Code highlighting
--- lib/markdown-components.tsx
+++ lib/markdown-components.tsx
@@ -2,14 +2,42 @@ import Image from "next/image";
 import Link from "next/link";
 import { MarkdownComponentType } from "./types";
 import { CodeProps } from "react-markdown/lib/ast-to-react";
-import CodeHighlight from "@components/code-highlight";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import dark from "react-syntax-highlighter/dist/cjs/styles/prism/night-owl";
+
+import tsx from "react-syntax-highlighter/dist/cjs/languages/prism/tsx";
+import typescript from "react-syntax-highlighter/dist/cjs/languages/prism/typescript";
+import scss from "react-syntax-highlighter/dist/cjs/languages/prism/scss";
+import bash from "react-syntax-highlighter/dist/cjs/languages/prism/bash";
+import markdown from "react-syntax-highlighter/dist/cjs/languages/prism/markdown";
+import json from "react-syntax-highlighter/dist/cjs/languages/prism/json";
+
+SyntaxHighlighter.registerLanguage("tsx", tsx);
+SyntaxHighlighter.registerLanguage("ts", typescript);
+SyntaxHighlighter.registerLanguage("typescript", typescript);
+SyntaxHighlighter.registerLanguage("scss", scss);
+SyntaxHighlighter.registerLanguage("bash", bash);
+SyntaxHighlighter.registerLanguage("markdown", markdown);
+SyntaxHighlighter.registerLanguage("json", json);
 
 const components = {
-  code: ({ node, inline, className, ...props }: CodeProps) => {
+  code: ({ node, className, ...props }: CodeProps) => {
     const match = /language-(\w+)/.exec(className || "");
 
     return match ? (
-      <CodeHighlight match={match[1]} {...props} />
+      <SyntaxHighlighter
+        // @ts-ignore
+        style={dark}
+        customStyle={{
+          fontSize: "14px",
+          borderRadius: "4px",
+        }}
+        language={match[1]}
+        PreTag="div"
+        wrapLines={true}
+        useInlineStyles={true}
+        {...props}
+      />
     ) : (
       <code className={className} {...props} />
     );
